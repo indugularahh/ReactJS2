527b707af634f4724ea32eb17739ba9540e0680d
indugularahh <indugularahh@gmail.com>
2021-07-05 20:44:03 +0530

    Add project files.

---
+++ JavaScriptReactHTMLApp2/app.js
@@ -0,0 +1,27 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+class Greeter extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { time: new Date() };
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.setState({ time: new Date() }), 500);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.message} <span>{this.state.time.toUTCString()}</span>
+            </div>
+        );
+    }
+}
+ReactDOM.render(<Greeter message="The time is: " />, document.getElementById("root"));
